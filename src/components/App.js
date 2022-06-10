401da2287ada0bb003a0babeef56386adda310d9
kesel <a.kiselyoff@gmail.com>
2022-06-10 14:29:04 +0300

    step#1 done

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,27 @@ import s from './containerApp.module.css';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    console.log(parsedContacts);
+
+    this.setState({ contacts: parsedContacts });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      console.log('contacts was update');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   handleFilterChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
